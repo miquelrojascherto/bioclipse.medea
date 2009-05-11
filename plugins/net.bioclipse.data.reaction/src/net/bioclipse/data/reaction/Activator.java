479323dc2047f9c0b62708faa7d3496a4f374223
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-11 13:35:07 +0000

    Initial import.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10220 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.data.reaction/src/net/bioclipse/data/reaction/Activator.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2008 The Bioclipse Project and others.
+ *               2009      Egon Willighagen
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org�epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contributors:
+ *     Stefan Kuhn, Miguel Rojas
+ ******************************************************************************/
+package net.bioclipse.data.reaction;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ */
+public class Activator extends AbstractUIPlugin {
+
+    public static final String PLUGIN_ID = "net.bioclipse.data.reaction";
+
+    private static Activator plugin;
+    
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
+
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+}
