0c3a6188bf97db178ff3e37e91d0defbb1de8cc3
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-12 12:30:23 +0000

    reaction editor roughly working
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10249 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.reaction/src/net/bioclipse/plugins/bc_reaction/model/ArrowConnectionModel.java
+++
@@ -1,8 +0,0 @@
-package net.bioclipse.plugins.bc_reaction.model;
-/**
- * 
- * @author Miguel Rojas
- */
-public class ArrowConnectionModel extends AbstractConnectionModel{
-
-}
--- plugins/net.bioclipse.reaction/src/net/bioclipse/plugins/bc_reaction/model/LineConnectionModel.java
+++
@@ -1,8 +0,0 @@
-package net.bioclipse.plugins.bc_reaction.model;
-/**
- * 
- * @author Miguel Rojas
- */
-public class LineConnectionModel extends AbstractConnectionModel{
-
-}
