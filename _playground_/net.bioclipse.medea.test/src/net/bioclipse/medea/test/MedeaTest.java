ef71f17413190189128f6753148f2a90d0d8e59e
miguelrojasch <miguelrojasch@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-13 10:29:03 +0000

    Added test for learning process in Medea
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10294 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- _playground_/net.bioclipse.medea.test/src/net/bioclipse/medea/test/MedeaTest.java
+++ _playground_/net.bioclipse.medea.test/src/net/bioclipse/medea/test/MedeaTest.java
@@ -14,7 +14,12 @@ package net.bioclipse.medea.test;
 
 import java.io.InputStream;
 
+import net.bioclipse.core.MockIFile;
+import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.medea.business.MedeaManager;
 import net.bioclipse.plugins.medea.core.Medea;
+import net.bioclipse.spectrum.business.SpectrumManager;
+import net.bioclipse.spectrum.domain.IJumboSpectrum;
 
 import org.junit.Test;
 import org.openscience.cdk.exception.CDKException;
@@ -26,7 +31,17 @@ import org.xmlcml.cml.element.CMLSpectrum;
 
 public class MedeaTest {
 
-    @Test public void testGenerating() throws Exception {
+	private MedeaManager medeamanager;
+	
+	public MedeaTest(){
+		medeamanager = new MedeaManager();		    
+	}
+    
+	public IBioclipseManager getManager() {
+        return medeamanager;
+    }
+
+	@Test public void testPredicting() throws Exception {
     	InputStream ins = MedeaTest.class.getResourceAsStream("/testFiles/30460-92-5-2d.mol");
         MDLV2000Reader reader = new MDLV2000Reader(ins);		
 		IChemFile chemFile = new org.openscience.cdk.ChemFile();
@@ -41,6 +56,28 @@ public class MedeaTest {
         Medea medea = new Medea();
         medea.predictMS(container);
         CMLSpectrum cml = medea.getPredictedSpectrum();
+        System.out.println(cml.toXML());
+        
+    }
+    @Test public void testLearning() throws Exception {
+    	InputStream ins = MedeaTest.class.getResourceAsStream("/testFiles/30460-92-5-2d.mol");
+        MDLV2000Reader reader = new MDLV2000Reader(ins);		
+		IChemFile chemFile = new org.openscience.cdk.ChemFile();
+        try {
+            chemFile=(IChemFile)reader.read(chemFile);
+        } catch (CDKException e) {
+        	e.printStackTrace();
+        }
+        
+        IAtomContainer container = ChemFileManipulator.getAllAtomContainers(chemFile).get(0);
+        String path = getClass().getResource("/testFiles/30460-92-5.jdx").getPath();
+
+        SpectrumManager spectrummanager = new SpectrumManager();
+        IJumboSpectrum spectrum = (IJumboSpectrum)spectrummanager.loadSpectrum( new MockIFile(path));
+//        Medea medea = new Medea();
+//        medea.learningMS(container, spectrum.getJumboObject(), "tet");
+//        CMLSpectrum cml = medea.getPredictedSpectrum();
+//        System.out.println(cml.toXML());
         
     }
 
