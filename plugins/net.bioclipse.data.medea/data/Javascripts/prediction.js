a6b1ee2c797d89ed23986e16461aecd8ee5e074f
Miguel Rojas Cherto <miguelrojasch@gmail.com>
2009-07-21 17:09:19 +0200

    adapted to return ICDKReactionScheme

--- plugins/net.bioclipse.data.medea/data/Javascripts/prediction.js
+++ plugins/net.bioclipse.data.medea/data/Javascripts/prediction.js
@@ -5,6 +5,6 @@ mol = cdk.loadMolecule("/Sample Data/Molecules/30460-92-5-2d.mol");
 mol = cdk.generate2dCoordinates(mol);
 ui.open(mol);
 
-cmlSpectrum = medea.predictMassSpectrum(mol)
-spectrum.saveSpectrum(cmlSpectrum,"Virtual/predictedSpectrum.cml","cml")
-ui.open("/Virtual/predictedSpectrum.cml");
+cmlReactionScheme = medea.predictMassSpectrum(mol)
+reaction.saveReactionScheme(cmlReactionScheme,"Virtual/predictedFragmenation.cml")
+ui.open("/Virtual/predictedFragmenation.cml");
