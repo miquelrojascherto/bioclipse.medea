b7c3cd83fe22531f3b0a58b02fcaf23a4470835b
Miguel Rojas Cherto <miguelrojasch@gmail.com>
2009-07-21 10:28:37 +0200

    set the javascript for looping sdf files to predict the mass spectrum

---
+++ plugins/net.bioclipse.data.medea/data/Javascripts/predictionSDF.js
@@ -0,0 +1,14 @@
+//Demonstrates basic manipulation to predict a EI-MS in Bioclipse using
+//a sdf file containing a list of molecule. 
+//Requires medea data installed in default location
+var mols = cdk.loadMolecules("/Sample Data/SDFfiles/CAS.5.sdf");
+for (var i = 0; i < mols.size(); i++) {
+	var mol = mols.get(i);
+	mol = cdk.generate2dCoordinates(mol);
+	ui.open(mol);
+
+	cmlSpectrum = medea.predictMassSpectrum(mol)
+	spectrum.saveSpectrum(cmlSpectrum,"Virtual/predictedSpectrum_"+i+".cml","cml")
+//	ui.open("/Virtual/predictedSpectrum_"++".cml");
+}
+
